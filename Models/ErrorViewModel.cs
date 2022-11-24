9a10e6c03ebbdf2e8b6fa5f705e4e0f5b983b19b
Giann Honorato <99771084+h0noratogi4nn@users.noreply.github.com>
2022-11-24 20:44:17 -0300

    completo

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Projeto_turmaT23.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
